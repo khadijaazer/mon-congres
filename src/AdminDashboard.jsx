d5e339eb7ccf465783d5ae6845ba82b31d7f16c0
Khadidja Abderrahmane <khadidjaazer1234@gmail.com>
2025-12-17 18:01:10 +0100

    Modifications

--- src/AdminDashboard.jsx
+++ src/AdminDashboard.jsx
@@ -14,7 +14,7 @@ const AdminDashboard = () => {
 
   // --- CONFIGURATION SÉCURITÉ ---
   // Remplace ceci par l'email EXACT de ton client
-  const ALLOWED_EMAIL = "ton_client@gmail.com"; 
+  const ALLOWED_EMAIL = "khadidjaazer1234@gmail.com"; 
   
   const navigate = useNavigate();
 
